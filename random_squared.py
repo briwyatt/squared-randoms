aba614ef02fda3edb92c1869c8d4ea256725c139
Bri Wyatt <mbriwyatt@gmail.com>
2017-04-08 23:39:29 -0500

    inital commit

---
+++ random_squared.py
@@ -0,0 +1,14 @@
+#Using the random module and the range method, generate a list of 20 random numbers between 0 and 49.
+from random import randrange
+
+random_numbers =[]
+for i in range (10):  
+    random_numbers.append(randrange(0,49,1))
+print ("random_numbers:", random_numbers)
+
+#use a list comprehension to build a new list that contains each number squared.
+squared_list = []
+
+for n in random_numbers: 
+    squared_list.append(n*n)
+print("squared list of previous list:", squared_list)
